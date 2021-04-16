b810aa101402336fbb49634c62344cf51e560b2b
xixilight <1985601026@qq.com>
2021-04-16 10:31:06 +0800

    test markdown link

---
+++ src/algorithmn/test/Test.java
@@ -0,0 +1,57 @@
+package algorithmn.test;
+
+public class Test {
+//    public static void main(String[] args) {
+//        // 1
+//        int i1 = 10;
+//        i1++;
+//        System.out.println(i1); // 11
+//
+//        int i2 = 10;
+//        ++i2;
+//        System.out.println(i2);  //11
+//
+//        // 2
+//        int i3 = 10;
+//        int i4 = i3++;
+//        System.out.println(i4); // 10
+//
+//        int i5 = 10;
+//        int i6 = ++i5;
+//        System.out.println(i6); // 11
+//
+//        // 3
+//        int i7 = 10;
+//        i7 = ++i7;
+//        System.out.println(i7); // 11
+//
+//        int i8 = 10;
+//        i8 = i8++;
+//        System.out.println(i8); // 10
+//
+//        // 4
+//        int i9 = 10;
+//        int i10 = i9++ + ++i9;
+//        System.out.println(i10); // 22
+//
+//        int i11 = 10;
+//        int i12 = ++i11 + i11++;
+//        System.out.println(i12); // 22
+//    }
+
+    public static void main(String[] args) {
+        new Test().A();
+    }
+
+    public void A() {
+//        int m = 0;
+        int k = Book();
+        int n = 0;
+    }
+
+    public int Book() {
+        return 5;
+    }
+
+
+}
